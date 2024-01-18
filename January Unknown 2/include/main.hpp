5a3e2da37752271f675773d164df145148bfaf60
Stöpa <stopa.averyanov@gmail.com>
2024-01-18 10:08:08 +0300

    Revert "Add source code for jan unknown 2"

--- January Unknown 2/include/main.hpp
+++
@@ -1,5 +0,0 @@
-#include "write_read.hpp"
-#include <string>
-#include "translate.hpp"
-
-int main();
--- January Unknown 2/include/translate.hpp
+++
@@ -1,5 +0,0 @@
-#include <map>
-#include <string>
-
-std::string encode_string(std::string input);
-std::string encode_character(char input);
